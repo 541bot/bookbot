fd8c49edf2af0b65a59910733ce1a98ee8d894d3
541bot <tobias_richter@outlook.com>
2025-03-18 22:12:37 +0100

    book no longer hardcoded, can be specified from the command line.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from stats import count_words
 from stats import count_chars
 from stats import sort_char_count
+import sys
 
 def get_book_text(file):
     return file.read()
 
 def main():
-    book_source = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_source = sys.argv[1]    
+
     with open(book_source) as f:
         text = get_book_text(f)
         num_words = count_words(text)
         num_chars = count_chars(text)
         sorted_count = sort_char_count(num_chars)
-        # print(f"{num_words} words found in the document")
-        # print(num_chars)
         print("============ BOOKBOT ============")
         print(f"Analyzing book found at {book_source}...")
         print("----------- Word Count ----------")
